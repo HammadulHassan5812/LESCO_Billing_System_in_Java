f2470ee034106387f23e2d21a53ec78767172914
HammadulHassan5812 <hammadulhassan2620@gmail.com>
2024-09-14 21:48:32 +0500

    created customer object

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import  java.util.List;
 
 public class Main {
 
@@ -6,19 +6,26 @@ public class Main {
     {
         Employee e = new Employee("C:\\Users\\city\\Desktop\\java\\LESCO_Billing_System_in_Java\\data\\EmployeesData.txt");
         e.load_EmployeeData();
+        List<Customer> customers =Customer.loadCustomerData("C:\\Users\\city\\Desktop\\java\\LESCO_Billing_System_in_Java\\data\\Customerinfo.txt");
 
 
 
+//         //Test login
+//        if (e.is_Employee()) {
+//            System.out.println("Login successful!");
+//            e.Change_password();
+//        } else {
+//            System.out.println("Wrong Username or Password");
+//        }
 
-         //Test login
-        if (e.is_Employee()) {
-            System.out.println("Login successful!");
-            e.Change_password();
-        } else {
-            System.out.println("Wrong Username or Password");
-        }
 
-
+//
+//        // Display all customer data
+//        for (Customer customer : customers) {
+//            customer.displayCustomerInfo();
+//        }
+//
+//
 
 
 

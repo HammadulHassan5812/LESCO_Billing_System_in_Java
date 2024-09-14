cfbd748eec9f3b34b27a42c5ea67f84df991f371
HammadulHassan5812 <hammadulhassan2620@gmail.com>
2024-09-14 19:31:03 +0500

    fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Employee e = new Employee();
-        e.load_EmployeeData("C:\\Users\\city\\Desktop\\java\\LESCO_Billing_System_in_Java\\data\\EmployeesData.txt");
+        Employee e = new Employee("C:\\Users\\city\\Desktop\\java\\LESCO_Billing_System_in_Java\\data\\EmployeesData.txt");
+        e.load_EmployeeData();
 
-        // Test login
-//        if (e.is_Employee()) {
-//            System.out.println("Login successful!");
-//        } else {
-//            System.out.println("Wrong Username or Password");
-//        }
-//
-//        // Test password change
-//        e.Change_password();
-        e.displayEmployees();
+
+
+
+         //Test login
+        if (e.is_Employee()) {
+            System.out.println("Login successful!");
+            e.Change_password();
+        } else {
+            System.out.println("Wrong Username or Password");
+        }
+
+
+
 
 
     }
